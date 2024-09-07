67e241339dd7ef6db881ea006377ff4a693a508e
stefaniemiranda1 <stefaniedmiranda@gmail.com>
2024-09-07 19:44:42 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,158 @@
+let dados = [
+
+{
+    titulo: "Emagrecer",
+    descricao: "Emagrecer é um objetivo comum para muitas pessoas, e alcançar esse objetivo de forma saudável e sustentável requer uma combinação de dieta equilibrada, atividade física regular e hábitos de vida saudáveis. O segredo para emagrecer está em consumir menos calorias do que você gasta. Você pode calcular seu consumo calórico diário para entender melhor. Lembre-se: se o Pikachu conseguiu, você também consegue alcançar seus objetivos! Acredite em seu potencial e celebre cada pequena conquista.",
+    imagem: "imagem/_pikachu.PNG", // Substitua pelo caminho da sua imagem
+    link: "https://pt.wikipedia.org/wiki/Perda_de_peso#:~:text=Efeitos%20na%20sa%C3%BAde,-A%20obesidade%20aumenta&text=A%20redu%C3%A7%C3%A3o%20da%20obesidade%20reduz,indiv%C3%ADduos%20com%20sobrepeso%20e%20obesos",
+    tags: "exercício cardio queimar calorias saúde Aeróbico Exercicio perder  "
+},
+
+{
+
+    titulo: "Manter o Peso",
+    descricao: "Manter o peso ideal é super importante. Uma alimentação equilibrada e a prática regular de exercícios físicos são fundamentais para garantir a saúde e o bem-estar a longo prazo. Ao manter um peso saudável, você reduz o risco de diversas doenças. Seria bom se pudéssemos comer como o Luffy, mas infelizmente não é assim. Portanto, devemos nos alimentar de forma equilibrada.",
+    link: "https://www.unimedfortaleza.com.br/blog/alimentacao/dicas-praticas-para-manter-o-peso-ideal",
+    imagem: "imagem/1_Luffy.PNG", 
+    tags: "manter equilíbrio  "
+  },
+
+{
+    titulo: "Ganhar Massa Magra",
+    descricao: "Ganhar massa magra é um objetivo comum para quem busca um corpo mais definido e forte. Para alcançar esse objetivo, é fundamental combinar uma dieta rica em proteínas com exercícios de força. O treino de resistência é essencial para estimular o crescimento muscular, bora treinar ",
+    
+    link: "https://www.minhavida.com.br/bem-estar/fitness/como-ganhar-massa-muscular",
+    imagem: "imagem/2_Majin_Boo.png", 
+    link1: "https://oglobo.globo.com/saude/bem-estar/noticia/2023/07/27/dieta-para-ganhar-massa-muscular-veja-6-dicas-e-alimentos.ghtml",
+    tags: "musculação hipertrofia  treino força "
+  },
+
+
+
+    {
+         titulo: "Açúcar",
+         descricao: "O açúcar, em sua forma mais comum, é um carboidrato cristalizado comestível, principalmente a sacarose. Ele pode ser obtido a partir da cana-de-açúcar ou da beterraba. O mais consumido no Brasil é o açúcar cristal, e deve ser consumido com moderação em dietas.",
+         link: "https://pt.wikipedia.org/wiki/A%C3%A7%C3%BAcar",
+         imagem: "imagem/acucar_tabela.png",
+         tags:" doce sobremesa glicose acucar"   
+    
+
+    },
+
+    {   
+        titulo: "Arroz",
+        descricao: "O arroz é um cereal amplamente consumido em todo o mundo, rico em carboidratos e fonte de energia. Existem diversas variedades de arroz, como o branco, integral, arbóreo e selvagem, cada uma com suas características nutricionais e culinárias. O arroz é um alimento versátil e base da alimentação em muitas culturas.",
+        link: "https://pt.wikipedia.org/wiki/Arroz",
+        imagem: "imagem/arroz_tabela.PNG", 
+        tags: "cereal carboidrato  arroz branco arroz integral"
+    },
+
+
+
+
+    {
+        titulo: "Carne Bovina",
+        descricao: "A carne bovina é uma excelente fonte de proteína, ferro e vitaminas do complexo B. É um alimento versátil, utilizado em diversas preparações culinárias e fundamental para uma dieta equilibrada. No entanto, o consumo excessivo de carnes vermelhas pode estar associado a alguns riscos à saúde, como doenças cardiovasculares.",
+        link: "https://pt.wikipedia.org/wiki/Carne_bovina",
+        imagem: "imagem/Carne bovina_tabela.PNG",
+        tags: "carne proteína, ferro, vitaminaB12, nutrição, churrasco, bife proteina"
+    },   
+{
+    titulo: "Feijão",
+    descricao: "O feijão é uma leguminosa rica em proteínas, fibras e ferro. É um alimento básico em muitas culturas e um componente fundamental de uma dieta equilibrada. O feijão auxilia na saciedade, regula os níveis de açúcar no sangue e contribui para a saúde intestinal.",
+    link: "https://pt.wikipedia.org/wiki/Feijão",
+    imagem: "imagem/feijão_tabela.PNG",
+    tags: "feijao leguminosa proteína fibra ferro proteina"
+},
+
+{
+titulo: "Frango",
+descricao: "O frango é uma excelente fonte de proteína de alta qualidade, com baixo teor de gordura saturada. É um alimento versátil, utilizado em diversas preparações culinárias e pode ser incluído em dietas variadas. O frango contribui para a construção e reparação dos tecidos musculares e fortalece o sistema imunológico.",
+link: "https://pt.wikipedia.org/wiki/frango",
+imagem: "imagem/frango_tabela.png",
+tags:" frango proteína carne branca grelhado assado proteina "
+
+
+},
+
+{   titulo: "Fruta",
+    descricao: "Frutas como banana, laranja, maçã e manga são pilares de uma alimentação saudável e variada e são as frutas mais consumidas no Brasil. Essas delícias da natureza são repletas de nutrientes essenciais, como vitaminas, minerais e fibras, que contribuem para o bom funcionamento do organismo. Ao incluir essas frutas no dia a dia, você fortalece o sistema imunológico, regula o intestino e obtém a energia necessária para um dia ativo.",
+    link: "https://pt.wikipedia.org/wiki/Fruta",
+    imagem: "imagem/frutas_tabela.png",
+    tags: "saudável vitamina fibra natural banana laranja maçã manga fruta tropical fruta cítrica"
+},
+
+{
+    titulo: "Fubá",
+    descricao: "O fubá promove a saciedade e regula o intestino, sendo uma excelente opção para quem busca uma alimentação equilibrada. Seus carboidratos complexos liberam energia de forma gradual, evitando picos de açúcar no sangue. Além disso, o fubá é uma fonte de vitaminas e minerais essenciais, como as do complexo B, ferro, zinco e magnésio.", 
+    link: "https://pt.wikipedia.org/wiki/Fubá", 
+    imagem: "imagem/fuba_tabela.png", 
+    tags: "bolo cuscuz massa fuba"
+
+},
+{
+
+    titulo: "Legumes",
+    descricao: "Legumes são a parte boa das plantas! Cheios de vitaminas, minerais e fibras, eles são ótimos para a saúde. Tomate, cenoura e brócolis são só alguns exemplos que você pode comer à vontade. ",
+    link: "https://pt.wikipedia.org/wiki/Legume",
+    imagem: "imagem/legumes_tabela.png", 
+    tags: "saudável vegetal fibra vitamina alface tomate pepino  cenoura brocolis espinafre brócolis "
+},
+{
+    titulo: "Óleo",
+    descricao: "Os óleos, extraídos de diversas fontes vegetais e animais, são gorduras líquidas à temperatura ambiente e uma rica fonte de energia. A composição em ácidos graxos varia entre os tipos (como soja, oliva, canola, milho e coco), influenciando diretamente seus benefícios e malefícios à saúde. Embora essenciais para a absorção de vitaminas, o consumo excessivo pode levar ao ganho de peso devido ao alto teor calórico. É fundamental escolher óleos de boa qualidade e utilizá-los com moderação.",
+    link: "https://pt.wikipedia.org/wiki/Óleo_comestível", 
+    imagem: "imagem/oleo_tabela.png", 
+    tags: "gordura culinária cozinhar oleo soja oliva canola milho  coco"
+},
+
+{
+    titulo: "Ovo",
+    descricao: "Ovo é um alimento completo, rico em proteínas, vitaminas e minerais. É uma excelente fonte de vitamina D, essencial para a saúde dos ossos e do sistema imunológico. Além disso, contém colina, um nutriente importante para o desenvolvimento do cérebro e das funções cognitivas.",
+    link: "https://pt.wikipedia.org/wiki/Ovo",
+    imagem: "imagem/ovo_tabela.png",
+    tags: "proteína galinha proteina" 
+},
+
+
+{
+    titulo: "Pão",
+    descricao: "O pão, um alimento presente em diversas culturas, apresenta diferentes variedades. O pão francês, leve e crocante, é elaborado com farinha de trigo refinada, água, fermento e sal. Já o pão de forma integral, mais nutritivo, é produzido com farinha de trigo integral, rica em fibras e outros nutrientes. Enquanto o pão francês é uma opção clássica, o pão integral é uma escolha mais saudável devido ao seu conteúdo de fibras, que auxiliam na digestão e promovem a saciedade.",
+    link: "https://pt.wikipedia.org/wiki/Pão", 
+    imagem: "imagem/pão_tabela.png", 
+    tags: "pao carboidrato farinha crocante integral fibra"
+},
+
+
+   {
+    titulo: "Peixe",
+    descricao: "O peixe, um alimento versátil e nutritivo, é uma excelente fonte de proteínas de alta qualidade, vitaminas do complexo B e minerais como fósforo e selênio. Entre as diversas espécies, destacam-se o salmão, o atum, a sardinha, o bacalhau e o linguado, cada um com suas características nutricionais e culinárias. Além de ser uma carne branca magra, o peixe é rico em ômega-3, ácidos graxos essenciais que auxiliam na saúde do coração e do cérebro. O salmão, por exemplo, é conhecido por sua alta concentração de ômega-3, enquanto a sardinha é uma ótima fonte de cálcio. O bacalhau, por sua vez, é versátil e pode ser preparado de diversas formas, sendo uma excelente opção para dietas equilibradas.",
+    link: "https://pt.wikipedia.org/wiki/Peixe",
+    imagem: "imagem/peixe_tabela.png",
+    tags: "proteína marisco ômega3 salmão  atum sardinha bacalhau  linguado salmao"
+
+
+},
+
+
+{
+
+    titulo: "Queijo",
+    descricao: "O queijo, um alimento versátil e nutritivo, é obtido pela coagulação do leite de diversos animais, como vaca, cabra e ovelha. Rico em cálcio e proteínas, é uma excelente fonte de nutrientes e pode ser consumido puro ou em diversas preparações culinárias. A variedade de queijos é imensa, com sabores e texturas que atendem a todos os paladares. Entre os mais populares, destacam-se a ricota, a mussarela, o cheddar, cada um com suas características únicas. A ricota, por exemplo, é leve e cremosa, perfeita para recheios e saladas. A mussarela, elástica e saborosa, é ideal para pizzas e massas. Já o cheddar, com seu sabor intenso e textura firme, é utilizado em sanduíches, hambúrgueres e diversas outras receitas.",
+    link: "https://pt.wikipedia.org/wiki/Queijo",
+    imagem: "imagem/queijo_tabela.png", 
+    tags: "lácteo cálcio proteína  ricota  mussarela  cheddar proteina lacteo calcio"
+
+
+},
+
+{
+    titulo: "Refrigerante",
+    descricao: "O refrigerante, uma bebida carbonatada popular mundialmente, pode ser encontrado em duas versões principais: tradicional, adoçada com açúcar, e zero, adoçado com edulcorantes artificiais. Ambas as versões contêm água carbonatada, corantes e aromatizantes, proporcionando um sabor refrescante e gasoso. O consumo excessivo de refrigerantes, tanto os tradicionais quanto os zero, está associado a diversos problemas de saúde, como obesidade, diabetes, cáries e doenças cardiovasculares. Embora os refrigerantes zero não contenham açúcar e, portanto, não contribuam diretamente para o ganho de peso, estudos sugerem que o consumo regular dessas bebidas pode alterar o paladar, levando ao consumo de alimentos mais doces e calóricos. Além disso, alguns estudos associam o consumo de edulcorantes artificiais a um maior risco de desenvolver diabetes tipo 2.",
+    link: "https://pt.wikipedia.org/wiki/Refrigerante",
+    imagem: "imagem/refri_tabela.png", 
+    tags: "bebida açúcar carbonatado  adoçante refri zero "
+}
+
+
+];
